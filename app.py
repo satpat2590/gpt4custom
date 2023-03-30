c1357c05ba7a7ba962409f9513639a8482b1e41b
Satyam Patel <satya@dyn198132.cc.lehigh.edu>
2023-03-30 13:42:21 -0400

    adding revised files

--- app.py
+++ app.py
@@ -1,35 +1,42 @@
+import os
+import PyPDF2
+from bs4 import BeautifulSoup
 from flask import Flask, request, render_template
 import openai
 
 app = Flask(__name__)
 
-
-# # Replace 'your_api_key' with your actual API key
-openai.api_key = "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"
-
-# # Set the model and other parameters
-model = "gpt-4"  # Replace with the appropriate model version
-max_tokens = 100
-
 @app.route('/', methods=['GET', 'POST'])
 def chat_gpt():
     if request.method == 'POST':
-        document = request.form['document']
+        document_file = request.files['document']
         query = request.form['query']
-        answer = get_answer_from_gpt(document, query)
+        document_text = extract_text_from_file(document_file)
+        answer = get_answer_from_gpt(document_text, query)
         return render_template('index.html', answer=answer)
     return render_template('index.html', answer=None)
 
-def get_answer_from_gpt(document, query):
-    prompt = f"{document}\n\nQuestion: {query}\nAnswer:"
+def extract_text_from_file(document_file):
+    file_extension = os.path.splitext(document_file.filename)[1].lower()
+    text = ""
 
-    response = openai.Completion.create(
-        engine=model,
-        prompt=prompt,
-        max_tokens=max_tokens,
-    )
-    generated_text = response.choices[0].text.strip()
-    return generated_text
+    if file_extension == '.txt':
+        text = document_file.read().decode('utf-8')
+    elif file_extension == '.pdf':
+        with PyPDF2.PdfFileReader(document_file) as pdf_reader:
+            for page_num in range(pdf_reader.numPages):
+                text += pdf_reader.getPage(page_num).extractText()
+    elif file_extension == '.html':
+        soup = BeautifulSoup(document_file.read(), 'html.parser')
+        text = soup.get_text()
+
+    return text
+
+def get_answer_from_gpt(document, query):
+    # Replace this with the actual API call to GPT-4
+    # and return the answer based on the document and query.
+    return "This is a sample answer."
 
 if __name__ == '__main__':
     app.run(debug=True)
+
